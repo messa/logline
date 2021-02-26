b63232588abb265cdc529e9eb03a7d21315fcf35
Petr Messner <petr.messner@gmail.com>
2021-02-26 15:13:00 +0100

    Agent: support asyncio debug mode in the Python 3.5 polyfill code

--- agent/logline_agent/asyncio_helpers.py
+++ agent/logline_agent/asyncio_helpers.py
@@ -7,9 +7,11 @@ logger = getLogger(__name__)
 try:
     from asyncio import run
 except ImportError:
-    def run(coro):
+    def run(coro, debug=False):
         from asyncio import get_event_loop
         loop = get_event_loop()
+        if debug:
+            loop.set_debug(True)
         loop.run_until_complete(coro)
 
 
